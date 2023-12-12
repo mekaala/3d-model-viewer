b5b071351e117f27c902d053ee87e93af2c5aeae
mekaala <mekaala@gmail.com>
2023-12-12 16:25:19 -0500

    added model component into Scene file assist with rendering component in scene properly

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,27 +1,52 @@
-import { OrbitControls, useHelper } from "@react-three/drei";
-import { useRef } from "react";
+import { Clone, OrbitControls, useGLTF, useHelper } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
 import { DirectionalLightHelper } from "three";
-import TorusKnot from "../shapes/TorusKnot";
+// import Model from '../shapes/Model'
+
+const Model = ({url, position, scale}) => {
+
+    const { scene } = useGLTF(url);
+
+    const ref = useRef();
+    const [isHovered, setIsHovered] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+
+
+    useFrame((state, delta) => {
+        const speed = isHovered ? 1 : 0
+        ref.current.rotation.y += delta * speed
+    })
+
+  return (
+    <Suspense>
+        <primitive
+            ref={ref}
+            position={position}
+            onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+            onPointerLeave={() => setIsHovered(false)}
+            object={scene}
+            onClick={() => setIsClicked(!isClicked)}
+            scale={isClicked ? 4.5 : 3}
+        />
+    </Suspense>
+  )
+}
 
 const Scene = () => {
     const directionalLightRef = useRef()
+
   
     useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "white");
     return (
       <>
-        <ambientLight intensity={0.1}/>
-        <directionalLight position={[0, 0, 2]} intensity={0.5} ref={directionalLightRef}/>
-        {/* <group position={[0, -1, 0]}>
-          <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]}/>
-          <Cube position={[-1, 0, 0]} color={"red"} size={[1, 1, 1]}/>
-          <Cube position={[-1, 2, 0]} color={"yellow"} size={[1, 1, 1]}/>
-          <Cube position={[1, 2, 0]} color={"blue"} size={[1, 1, 1]}/>
-        </group> */}
-        {/* <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"red"} />
-        <Sphere position={[0, 0, 0]} args={[1, 30, 30]} color={"red"}/>
-        <Torus position={[2, 0, 0]} size={[0.5, 0.1, 30, 30]} color={"blue"} /> */}
-        <TorusKnot position={[0, 0, 0]} size={[1, 0.1, 1000, 50]} color={"yellow"} />
-        <OrbitControls enableZoom={false}  />
+        <ambientLight intensity={1}/>
+        <Model url={'RockPikminPlush.glb'} position={[1, 0, 0]} scale={[3, 3, 3]}/>
+        {/* <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"red"} /> */}
+        {/* <Sphere position={[0, 0, 0]} args={[1, 30, 30]} color={"red"}/> */}
+        {/* <Torus position={[2, 0, 0]} size={[0.5, 0.1, 30, 30]} color={"blue"} /> */}
+        {/* <TorusKnot position={[0, 0, 0]} size={[1, 0.1, 1000, 50]} color={"yellow"} /> */}
+        <OrbitControls/>
       </>
     )
 }
